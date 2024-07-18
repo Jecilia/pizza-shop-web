c7d59840810186b7065fab2fbec6e1ff12e63f72
Jecília Teixeira <111911408+Jecilia@users.noreply.github.com>
2024-07-18 21:34:36 +0100

    Toast notifications (sonner)

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,13 @@ import "@/global.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Toaster } from "sonner";
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | Pizza Shop" />
+      <Toaster richColors />
       <RouterProvider router={router} />
     </HelmetProvider>
   );
