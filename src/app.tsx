f3c92b4e1a6a246aebed49ec1f4bf35b1f8b829e
Jecília Teixeira <111911408+Jecilia@users.noreply.github.com>
2024-07-18 16:41:40 +0100

    React Helmet (SEO)

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,13 @@
 import "@/global.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza Shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
 }
