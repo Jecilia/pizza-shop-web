7ce67f7af598d47038b20e595523aedf7d8a1450
Jecília Teixeira <111911408+Jecilia@users.noreply.github.com>
2024-07-22 01:24:02 +0100

    App layout with header

---
+++ src/components/header.tsx
@@ -0,0 +1,25 @@
+import { Home, Pizza, UtensilsCrossed } from "lucide-react";
+import { Separator } from "./ui/separator";
+import { NavLink } from "./nav-link";
+
+export function Header() {
+  return (
+    <header className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <Pizza className="size-6" />
+        <Separator orientation="vertical" className="h-6" />
+
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <NavLink to={"/"}>
+            <Home className="size-4" />
+            Início
+          </NavLink>
+          <NavLink to={"/orders"}>
+            <UtensilsCrossed className="size-4" />
+            Pedidos
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+}
