536fd1b7ebc27136870f78b4c841d17196b04145
Jecília Teixeira <111911408+Jecilia@users.noreply.github.com>
2024-07-23 01:41:26 +0100

    Page: Order List

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,12 +4,16 @@ import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/_layout/app";
 import { AuthLayout } from "./pages/_layout/auth";
 import { SignUp } from "./pages/auth/sign-up";
+import { Orders } from "./pages/app/orders/orders";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    children: [{ path: "/", element: <Dashboard /> }],
+    children: [
+      { path: "/", element: <Dashboard /> },
+      { path: "/orders", element: <Orders /> },
+    ],
   },
   {
     path: "/",
