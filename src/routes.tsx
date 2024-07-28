3c6480b752d84761958a3ba784390ae4c723a9fd
Jecília Teixeira <111911408+Jecilia@users.noreply.github.com>
2024-07-28 01:04:25 +0100

    Page 404 (Not Found)

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,11 +5,13 @@ import { AppLayout } from "./pages/_layout/app";
 import { AuthLayout } from "./pages/_layout/auth";
 import { SignUp } from "./pages/auth/sign-up";
 import { Orders } from "./pages/app/orders/orders";
+import { NotFound } from "./pages/404";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: "/", element: <Dashboard /> },
       { path: "/orders", element: <Orders /> },
